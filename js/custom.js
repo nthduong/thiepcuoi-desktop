695882398cd89c4c097f5e50682ab084e23b3159
Duong <nguyenthaiduong17082003@gmail.com>
2026-01-13 17:08:13 +0700

    fix ui

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,31 @@
-// $(".slideshow__inner").slick({
-//     arrows: false,
-//     dots: true,
-//     // autoplay: true,
-// });
+$(document).ready(function () {
+    let music = $("#bgMusic")[0];
+    let isPlaying = false;
+
+    music.volume = 1;
+
+    function playMusic() {
+        music.play();
+        $("#musicIcon").attr("src", "./images/audio-on.svg");
+        isPlaying = true;
+    }
+
+    function pauseMusic() {
+        music.pause();
+        $("#musicIcon").attr("src", "./images/audio-off.svg");
+        isPlaying = false;
+    }
+
+    $(document).one("click scroll touchstart", function () {
+        playMusic();
+    });
+
+    $("#musicToggle").click(function (e) {
+        e.stopPropagation();
+        if (isPlaying) {
+            pauseMusic();
+        } else {
+            playMusic();
+        }
+    });
+});
